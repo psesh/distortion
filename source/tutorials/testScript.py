3cf75132356cf3f05870e224a4f2973c6ed4b415
TZagrodnik <tzagrodnik3@gatech.edu>
2023-07-12 16:14:07 -0400

    Ground Vortex Paper Fig10f Digitized, Working on Plot Function

--- source/tutorials/testScript.py
+++ source/tutorials/testScript.py
@@ -1,27 +1,41 @@
 import pandas as pd
 from distorted import Distortion
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
 
-csvDF = pd.read_csv('data/sample.csv')
+# csvDF = pd.read_csv('data/Ground vortex aerodynamics Figure10f.csv')
+csvDF = pd.read_csv('data/figure14.csv')
 distortionObj = Distortion(csvDF)
 
 
-#print(csvDF.getDataframe())
-#print(distortionObj.getDF())
+distortionObj.plot_quantity('Total Pressure')
 
-#print(distortionObj.get_circumferentialAverage())
-#print(distortionObj.get_radialAverage())
-#print(distortionObj.get_areaWeightedAverage())
-#print(distortionObj.ARP1420PFAVEqualRingArea())
-#print(distortionObj.ARP1420().to_string())
-print(distortionObj.RollsRoyceDC60())
+# print(csvDF.getDataframe())
+# print(distortionObj.getDF())
+
+# print(distortionObj.get_circumferentialAverage())
+# print(distortionObj.get_radialAverage())
+# print(distortionObj.get_areaWeightedAverage())
+# print(distortionObj.ARP1420PFAVEqualRingArea())
+# print(distortionObj.ARP1420().to_string())
+# print(distortionObj.RollsRoyceDC60())
+
+# x, y = distortionObj.getRingData(1)
+
+
+# plt.plot(x,y)
+# plt.show()
+# Graphing
 
 
 
-#x, y = distortionObj.getRingData(1)
 
-
-#plt.plot(x,y)
-#plt.show()
-
-#Graphing
+# def ringsEqualArea(outerRadius, numberOfRings):
+#     radiusMiddleCircle = outerRadius / np.sqrt(numberOfRings)
+#
+#     radiiList = [radiusMiddleCircle]
+#     for i in range(numberOfRings-1):
+#         radiiList = radiiList + [radiusMiddleCircle * np.sqrt(i + 2)]
+#     return radiiList
+# print(ringsEqualArea(1, 9))
